667174bffd30b0ab9fa03e494f821c97b647ee47
suhanilad <suhanilad14@gmail.com>
2025-07-15 20:41:10 +0530

    5

---
+++ test.py
@@ -0,0 +1,6 @@
+A,B = "2",3
+Txt="@"
+print(2*Txt*3)
+# output: @@@@@@
+
+print((A+Txt)*B)
---
+++ varriables.py
@@ -0,0 +1,23 @@
+name="suhani"
+age=19
+price = 23.09
+age1="true"
+age2= False
+
+print("My name is:", name )
+print(age  )
+print(type(name))
+print(type(price))
+print(type(age))
+print(type(age2))
+
+
+
+a=10
+b=10
+
+sum=(a+b)
+print(sum)
+
+diff=(a-b)
+print(diff)
