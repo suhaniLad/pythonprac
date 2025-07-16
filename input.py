b2866eee5a5bdbfa31b386bccb3ddf7f9176b495
suhanilad <suhanilad14@gmail.com>
2025-07-16 11:35:23 +0530

    6

---
+++ input.py
@@ -0,0 +1,2 @@
+age = int(input("Enter your age"))  
+price = float(input("Enter your price"))
--- test.py
+++ test.py
@@ -1,6 +1,40 @@
 A,B = "2",3
-Txt="@"
-print(2*Txt*3)
+Text="@"
+print(2*Text*3)
 # output: @@@@@@
 
-print((A+Txt)*B)
+print((A+Text)*B)
+# output: 2@2@2@
+          
+A,B = 2, 3
+C= 4
+print(A+B*C)
+# output: 14
+
+A,B= 10, 5.0
+C= A*B
+print(C)
+# output: 50.0
+
+A,B=1,2
+C=A/B
+print(C)
+
+
+A,B= 1.5,3
+C=A//B
+print(C, A/B)
+
+
+A,B= -5,2
+C=A%B
+print(C) 
+
+A,B= 5,2
+C=A%B
+print(C)
+
+A,B= 5,-2
+C=A%B
+print(C)
+
